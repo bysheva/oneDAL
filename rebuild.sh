1e0be96c27103e89c9c6da7ed1f3226116150959
Olga Razvenskaya <olga.razvenskaya@intel.com>
2020-08-06 17:11:48 +0300

    next

--- rebuild.sh
+++ rebuild.sh
@@ -4,7 +4,6 @@ source ../daal-infra/bin/env/lnx32e.sh build gnu
 export CC=$GCC_ROOT/bin/gcc
 export CXX=$GCC_ROOT/bin/g++
 export LIBRARY_PATH=${GCC_ROOT}/lib64:${LIBRARY_PATH}
-make -j32 daal_c REQDBG=yes REQCPU=avx512 CORE.ALGORITHMS.CUSTOM=pca COMPILER=gnu
-make -j32 onedal_c REQDBG=yes REQCPU=avx512 CORE.ALGORITHMS.CUSTOM=pca COMPILER=gnu
-make -j32 onedal_c REQDBG=yes REQCPU=avx512 CORE.ALGORITHMS.CUSTOM=jaccard COMPILER=gnu
+make -j24 daal_c REQDBG=yes REQCPU=avx512 CORE.ALGORITHMS.CUSTOM=cholesky COMPILER=gnu
+make -j24 onedal_c REQDBG=yes REQCPU=avx512 CORE.ALGORITHMS.CUSTOM=jaccard COMPILER=gnu
 
