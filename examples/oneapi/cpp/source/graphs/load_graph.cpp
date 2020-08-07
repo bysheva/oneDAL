d1d319db0e5c1987ec9f42049446834ae2795508
Olga Razvenskaya <olga.razvenskaya@intel.com>
2020-08-07 13:31:09 +0300

    added load_graph example

---
+++ examples/oneapi/cpp/source/graphs/load_graph.cpp
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright 2020 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+#include "oneapi/dal/util/load_graph.hpp"
+#include <iostream>
+#include "oneapi/dal/data/graph.hpp"
+#include "oneapi/dal/util/csv_data_source.hpp"
+
+using namespace oneapi::dal;
+using namespace oneapi::dal::preview;
+
+int main(int argc, char **argv) {
+    if (argc < 2)
+        return 0;
+    std::string filename = argv[1];
+    csv_data_source ds(filename);
+    edge_list_to_csr_descriptor d;
+    auto my_graph = load_graph<graph>(d, ds);
+
+    std::cout << "Graph is read: ";
+    std::cout << "# vertices is " << get_vertex_count(my_graph) << " ";
+    std::cout << "# edges is " << get_edge_count(my_graph) << std::endl;
+    return 0;
+}
